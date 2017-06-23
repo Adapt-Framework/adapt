f4c9a3413412512098dbeff9c1246db1cfc0c308
Matt Bruton <matt.bruton@gmail.com>
2017-06-23 10:29:42 +0100

    Updates

--- models/model_web_host.php
+++ models/model_web_host.php
@@ -1,8 +1,77 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * The MIT License (MIT)
+ *   
+ * Copyright (c) 2017 Matt Bruton
+ * Authored by Matt Bruton (matt.bruton@gmail.com)
+ * http://www.adaptframework.com
+ *   
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *   
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *   
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *  
  */
 
+namespace adapt{
+    
+    /* Prevent direct access */
+    defined('ADAPT_STARTED') or die;
+    
+    class model_web_host extends model{
+        
+        const EVENT_ON_LOAD_BY_HOSTNAME = 'model_web_host.load_by_hostname';
+        
+        public function __construct($id = null, $data_source = null){
+            parent::__construct("bundle_version", $id, $data_source);
+        }
+        
+        public function load_by_hostname($hostname){
+            $this->initialise();
+            
+            if ($hostname){
+                $sql = $this->data_source->sql;
+                
+                $sql->select('*')
+                    ->from('web_host')
+                    ->where(
+                        new sql_and(
+                            new sql_cond('hostname', sql::EQUALS, sql::q($hostname)),
+                            new sql_cond('date_deleted', sql::IS, new sql_null())
+                        )
+                    );
+                
+                $results = $sql->execute(0)->results();
+                
+                if (count($results) == 1){
+                    $this->trigger(self::EVENT_ON_LOAD_BY_HOSTNAME);
+                    return $this->load_by_data($results[0]);
+                }elseif(count($results) == 0){
+                    $this->error("Unable to find a record");
+                }elseif(count($results) > 1){
+                    $this->error(count($results) . " records found, expecting 1.");
+                }
+            }
+            
+            $this->initialise();
+            return false;
+        }
+        
+        
+    }
+
+}
